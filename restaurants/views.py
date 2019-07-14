6972af67e58bc35daa21d054f1f0ee455ba8c1aa
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-14 20:09:03 +0300

    thanks Moe and Nouf

--- restaurants/views.py
+++ restaurants/views.py
@@ -31,10 +31,24 @@ def restaurant_create(request):
     return render(request, 'create.html', context)
 
 def restaurant_update(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    form = RestaurantForm(instance=restaurant)
 
-    return
+    if request.method == "POST":
+        form = RestaurantForm(request.POST, instance=restaurant)
+        if form.is_valid():
+            form.save()
+            return redirect("restaurant-list")
+
+    context = {
+        "form": form,
+        "restaurant": restaurant
+
+    }
+    return render (request, 'update.html', context)
+
 
 def restaurant_delete(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id).delete()
 
-
-    return
+    return redirect ('restaurant-list')
